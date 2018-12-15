dc279d9374b5c59b57474e8232f0034a734169c0
Angus Clark <angus.c@mmtdigital.co.uk>
2018-12-15 22:28:33 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
-  render() {
+
+  handleDragStart = (e) => {
+    console.info('start')
+  }
+
+  handleDragEnd = (e) => {
+  }
+
+  render () {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h1 className='header'> Drag and Drop </h1>
+      <ul className='box-container'>
+        <li className='box box-colored' draggable='true' onDragStart={this.handleDragStart} onDragEnd={this.handleDragEnd}> </li>
+        <li className='box'></li>
+        <li className='box'></li>
+        <li className='box'></li>
+        <li className='box'></li>
+      </ul>
       </div>
     );
   }
